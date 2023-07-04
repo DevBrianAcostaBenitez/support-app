8b3eed87195d295ead24a22a776f13dcf4e1d729
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-07-04 08:16:27 +0100

    proyect  created

---
+++ src/main/java/com/F5/supportapp/SupportAppApplication.java
@@ -0,0 +1,13 @@
+package com.F5.supportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/F5/supportapp/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.F5.supportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
