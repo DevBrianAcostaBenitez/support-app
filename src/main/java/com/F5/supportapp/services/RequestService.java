c1bb459560405385a52e1b2d04b44198efeb6e05
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-07-10 08:54:20 +0100

    controller and service updated: delete, find by id included and save updated

--- src/main/java/com/F5/supportapp/services/RequestService.java
+++ src/main/java/com/F5/supportapp/services/RequestService.java
@@ -1,8 +1,12 @@
 package com.F5.supportapp.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.F5.supportapp.models.Request;
 import com.F5.supportapp.repositories.RequestRepository;
@@ -17,7 +21,31 @@ public class RequestService {
         List<Request> requests = this.repository.findAll();
         return requests;
     }
-    public void save(Request request){
-        this.repository.save(request);
+    // public Request save(Request request){
+    //     this.repository.save(request);
+    //     return request;
+    // }
+    public Request save(Request request){
+        if (request.getName() == "") {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "name empty");
+        }
+
+        Request requestSaved = repository.save(request);
+        return requestSaved;
+    }
+    public Request findById(Long id) {
+        Request request = repository.findById(id).orElseThrow();
+        return request;
+    }
+
+    public Map<String, String> delete(Long id) {
+        Request request = repository.findById(id).orElseThrow();
+        repository.deleteById(request.getId());
+
+        Map<String,String> msg = new HashMap<>();
+        msg.put("message", "Country " + request.getName() + " successfully deleted!");
+
+        return msg;
+
     }
 }
